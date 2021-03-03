7b79dbbd00b0f0e8e3cad7b1b989f2e56ec7d33f
sanjay-ashok <78950931+sanjay-ashok@users.noreply.github.com>
2021-03-03 12:53:47 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -4,18 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Sanjay!! Welcome to Jenkins Pipeline")
-	addval := Add(4, 6)
-	fmt.Println("Example for Addition ==================> 4 + 6 = ", addval)
-	subval := Subtract(6, 4)
-	fmt.Println("Example for Subtraction ==================> 6 - 4 = ", subval)
-}
-
-// Add is our function that sums two integers
-func Add(x, y int) int {
-	return x + y
-}
-
-// Subtract subtracts two integers
-func Subtract(x, y int) int {
-	return x - y
 }
