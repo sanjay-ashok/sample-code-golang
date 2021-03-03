ff47284524c9945a7a6092f97b0568af69ccf282
sanjay-ashok <78950931+sanjay-ashok@users.noreply.github.com>
2021-03-03 12:43:48 +0530

    Update main.go

--- src/main.go
+++ src/main.go
@@ -5,3 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello Sanjay!! Welcome to Jenkins Pipeline")
 }
+
+// Add is our function that sums two integers
+func Add(x, y int) int {
+	return x + y
+}
+
+// Subtract subtracts two integers
+func Subtract(x, y int) int {
+	return x - y
+}
